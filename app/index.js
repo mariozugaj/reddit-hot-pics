70e0fde59f40bede7cf0429a15dea5d6c40c5bc8
Mario Zugaj <mario@mariozugaj.com>
2018-10-25 10:33:10 +0200

    Add Home screen template

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 
+import Home from "./screens/Home";
+
 export default class App extends React.Component {
   render() {
-    return <View />;
+    return <Home />;
   }
 }
