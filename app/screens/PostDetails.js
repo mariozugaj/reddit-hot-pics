6470175ac9e674fa8338fea044ccf6f9737ddaf9
Mario Zugaj <mario@mariozugaj.com>
2018-10-26 10:47:17 +0200

    Convert PostDetails screen into functional component + add propTypes

--- app/screens/PostDetails.js
+++ app/screens/PostDetails.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
-import { WebView, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import React from "react";
+import { WebView } from "react-native";
 
 import { AlertConsumer } from "../components/Alert";
 
-class PostDetails extends Component {
-  render() {
-    const { navigation } = this.props;
-    const postURL = navigation.getParam("postURL");
+const PostDetails = props => {
+  const { navigation } = props;
+  const postURL = navigation.getParam("postURL");
 
-    return <WebView source={{ uri: postURL }} />;
-  }
-}
+  return <WebView source={{ uri: postURL }} />;
+};
+
+PostDetails.propTypes = {
+  navigation: PropTypes.object,
+  alertWithType: PropTypes.func,
+  alert: PropTypes.func,
+};
 
 export default props => (
   <AlertConsumer>
